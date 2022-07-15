241435f65b0fb3056d4c82e7801eb32655f44a29
Ahmad Mana <Ahmadmanee2001@gmail.com>
2022-07-15 20:40:57 +0300

    secondary slider completed BY AHMAD MANA

--- assets/slider2.js
+++ assets/slider2.js
@@ -4,5 +4,14 @@ var children = slider2ImagesContainer.children
 for(var i=0; i<children.length; i++){
     console.log(children[i]);
     var s = children[i].clientWidth
-    children[i].style.left = s * i
+    children[i].style.left = ( (s+75) * i )
 }
+
+
+window.addEventListener('resize',(e)=>{
+    for(var i=0; i<children.length; i++){
+        console.log(children[i]);
+        var s = children[i].clientWidth
+        children[i].style.left = ( (s+75) * i )
+    }
+})
